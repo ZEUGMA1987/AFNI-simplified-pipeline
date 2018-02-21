7f3e3b903f5a57d318c48b52eec47ce096014c03
ZEUGMA1987 <rui.yuan@njit.edu>
2018-02-20 18:39:21 -0800

    Update submit_perl.pl

--- submit_perl.pl
+++ submit_perl.pl
@@ -1,12 +1,12 @@
 #!/bin/perl
 
 $job_name = 'preproc_AFNI' ;
-$job_output = '/oak/stanford/groups/menon/projects/ruiyuan/test_AFNI_preproc/job/test.%j.out' ;
-$job_error = '/oak/stanford/groups/menon/projects/ruiyuan/test_AFNI_preproc/job/test.%j.err' ;
+$job_output = '/oak/stanford/groups/XX/projects/SUNID/test_AFNI_preproc/job/test.%j.out' ;
+$job_error = '/oak/stanford/groups/XX/projects/SUNID/test_AFNI_preproc/job/test.%j.err' ;
 $job_time = '02:00:00' ;
-$input_dir = '/oak/stanford/groups/menon/rawdata/public/' ;
+$input_dir = '/oak/stanford/groups/XX/rawdata/public/' ;
 #$subID = '0051201'    ;
-$output_dir = '/oak/stanford/groups/menon/projects/ruiyuan/test_AFNI_preproc/data/imaging/participants'  ;
+$output_dir = '/oak/stanford/groups/XX/projects/SUNID/test_AFNI_preproc/data/imaging/participants'  ;
 
 
 chdir $input_dir;
@@ -19,7 +19,7 @@ foreach $subID (@list)
 
  chomp($subID);
  print "submitting ...\n";
- `sbatch -J $job_name -o $job_output -e $job_error -t $job_time --qos=normal -N 1 --mem-per-cpu=4000 -p menon --wrap=" /home/users/ruiyuan/test/preprocfMRI_AFNI.pl  $input_dir  $subID $output_dir " `;
+ `sbatch -J $job_name -o $job_output -e $job_error -t $job_time --qos=normal -N 1 --mem-per-cpu=4000 -p menon --wrap=" preprocfMRI_AFNI.pl  $input_dir  $subID $output_dir " `;
 
  print "$subID\n";
  #print $job
