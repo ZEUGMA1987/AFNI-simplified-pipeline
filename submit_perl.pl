2693f156ee8bbbb0ea9cba58e88c20f53bc8a36a
ZEUGMA1987 <rui.yuan@njit.edu>
2018-07-09 15:41:28 -0700

    Update submit_perl.pl

--- submit_perl.pl
+++ submit_perl.pl
@@ -19,7 +19,7 @@ foreach $subID (@list)
 
  chomp($subID);
  print "submitting ...\n";
- `sbatch -J $job_name -o $job_output -e $job_error -t $job_time --qos=normal -N 1 --mem-per-cpu=4000 -p menon --wrap=" preprocfMRI_AFNI.pl  $input_dir  $subID $output_dir " `;
+ `sbatch -J $job_name -o $job_output -e $job_error -t $job_time --qos=normal -N 1 --mem-per-cpu=4000 -p normal --wrap=" preprocfMRI_AFNI.pl  $input_dir  $subID $output_dir " `;
 
  print "$subID\n";
  #print $job
